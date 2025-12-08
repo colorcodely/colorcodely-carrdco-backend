a7cd32e14a3649c0a93da7da4ffbb288cfb2ed24
colorcodely <colorcodely@outlook.com>
2025-12-08 11:17:32 -0600

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,21 +1,21 @@
 from twilio.rest import Client
 import os
 
-# Environment variables you'll configure in Render
-TWILIO_SID = os.getenv("TWILIO_SID")
-TWILIO_AUTH = os.getenv("TWILIO_AUTH")
-TWILIO_FROM = os.getenv("TWILIO_FROM")
+# Environment variables from Render
+TWILIO_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_FROM = os.getenv("TWILIO_FROM_NUMBER")
 
-def send_sms(to_number, message):
-    """Send an SMS message using Twilio."""
-    try:
-        client = Client(TWILIO_SID, TWILIO_AUTH)
-        message = client.messages.create(
-            body=message,
-            from_=TWILIO_FROM,
-            to=to_number
-        )
-        print("SMS sent:", message.sid)
-    except Exception as e:
-        print("Error sending SMS:", e)
-        raise
+
+def send_sms(to_number: str, message: str):
+    """Send an SMS using Twilio."""
+    if not (TWILIO_SID and TWILIO_AUTH and TWILIO_FROM):
+        raise ValueError("Twilio environment variables are not fully set.")
+
+    client = Client(TWILIO_SID, TWILIO_AUTH)
+    msg = client.messages.create(
+        body=message,
+        from_=TWILIO_FROM,
+        to=to_number,
+    )
+    print("SMS sent:", msg.sid)
