0a908db941440705dd0f53bdbcedf3f3bbb2e210
colorcodely <colorcodely@outlook.com>
2025-12-12 13:37:59 -0600

    Create color_codes.py

---
+++ color_codes.py
@@ -0,0 +1,11 @@
+COLOR_CODES = [
+    "amber","apple","aqua","banana","beige","black","blue","bone","bronze",
+    "brown","burgundy","charcoal","chartreuse","cherry","chestnut","copper",
+    "coral","cream","creme","crimson","eggplant","emerald","fuchsia","ginger",
+    "gold","gray","green","hazel","indigo","ivory","jade","jasmine","khaki",
+    "lavender","lemon","lilac","lime","magenta","mahogany","maroon","mauve",
+    "mint","navy","olive","onyx","opal","orange","orchid","peach","pearl",
+    "periwinkle","pink","platinum","plum","purple","raspberry","red","rose",
+    "ruby","sage","sapphire","sienna","silver","tan","tangerine","teal",
+    "turquoise","vanilla","violet","watermelon","white","yellow"
+]
